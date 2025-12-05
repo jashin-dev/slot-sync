76ebc26c81f9d2f27a26de7009ed3a3a6fdea3c2
jashinbhatia <jashinbhatia4567@gmail.com>
2025-12-05 11:53:43 +0530

    header-footer-layout

---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className=" bg-gray-400 py-10">
+      <div className="container mx-auto text-center">This is the container for the footer</div>
+    </footer>
+  );
+}
+
+export default Footer;

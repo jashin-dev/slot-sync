d070fd90889dcacdaf040b9c1fde8238e2f1a3dc
jashinbhatia <jashinbhatia4567@gmail.com>
2025-12-06 14:37:42 +0530

    clerk auth implementation

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className=" bg-gray-400 py-10">
+    <footer className="bg-blue-100 py-10">
       <div className="container mx-auto text-center">This is the container for the footer</div>
     </footer>
   );
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
+import {SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
 import Link from "next/link";
 import Image from "next/image";
 import { PenBox } from "lucide-react";
+import UserMenu from "@/components/UserMenu";
 
 function Header() {
   return (
-    <nav className="px-4 py-2 flex justify-between items-center bg-gray-400">
+    <nav className="px-4 py-2 flex justify-between items-center shadow-md border-b-2">
       <Link href="/">
         <Image
-          src="/sync-slot-nav-logo.png"
+          src="/slot-sync-logo.png"
           alt="SyncSlot Logo"
           height={100}
           width={100}
@@ -23,7 +25,14 @@ function Header() {
             <span className="hidden sm:inline">Create Event</span>
           </Button>
         </Link>
-        <Button variant="default">Login</Button>
+        <SignedOut>
+          <SignInButton forceRedirectUrl="/dashboard">
+            <Button variant="default">Login</Button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserMenu />
+        </SignedIn>
       </div>
     </nav>
   );
